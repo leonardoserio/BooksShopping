c754dcfb9b243314979f948b55e26568d45885be
Leonardo <newmysms@gmail.com>
2021-03-06 14:23:18 -0300

    implements useNavigation onPress

--- src/components/ShoppingCartIcon.js
+++ src/components/ShoppingCartIcon.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { TouchableOpacity } from 'react-native';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
 
 function ShoppingCartIcon(props) {
   Ionicons.loadFont();
+  const navigation = useNavigation();
   return (
     <TouchableOpacity
-      onPress={() => alert('Press me')}
+      onPress={() => navigation.navigate('Cart')}
       style={{ marginRight: 10 }}
     >
       <Ionicons name="ios-cart" size={32} color="#101010" />
