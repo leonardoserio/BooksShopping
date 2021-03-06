53d4636b981b8316d611995d80dac66fcb3f85ff
Leonardo <newmysms@gmail.com>
2021-03-06 12:24:17 -0300

    create route screens

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import BookScreen from '../screens/BookScreen';
+import CartScreen from '../screens/CartScreen';
+
+const Stack = createStackNavigator();
+
+function MainStackNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Books" component={BookScreen} />
+        <Stack.Screen name="Cart" component={CartScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainStackNavigator;
