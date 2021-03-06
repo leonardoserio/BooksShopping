dd32dbf380151b26b97a51ab8a577a8a4b57e805
Leonardo <newmysms@gmail.com>
2021-03-06 12:19:18 -0300

    commit initial

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import MainStackNavigator from './src/navigation/AppNavigator';
+
+const App = () => {
+  return <MainStackNavigator />;
+};
+
+export default App;
