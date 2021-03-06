61dd7144b55d09ac3165c7e7338c7ca72ef8376f
Leonardo <newmysms@gmail.com>
2021-03-06 12:50:05 -0300

    custom icon

--- src/components/ShoppingCartIcon.js
+++ src/components/ShoppingCartIcon.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
 
-import Ionicons from 'react-native-vector-icons/FontAwesome'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 function ShoppingCartIcon(props) {
+  Ionicons.loadFont();
   return (
     <TouchableOpacity
       onPress={() => alert('Press me')}
-      style={{ maginRight: 10 }}
+      style={{ marginRight: 10 }}
     >
       <Ionicons name="ios-cart" size={32} color="#101010" />
     </TouchableOpacity>
   )
 }
+
+
+export default ShoppingCartIcon;
